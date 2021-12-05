0e1a4e190ebaffc472281d94e827611b76b728c1
nguyen19ok <95500554+nguyen19ok@users.noreply.github.com>
2021-12-05 15:49:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var io = socketIO(server,
 });
 var cuoc   = Array();
 var system = {
-	url : 'https://nguyen19.tk', 
+	url : 'http://nguyen19.freevar.com', 
 	keycode : '123456', 
 	ngocrong_min : 1000, 
 	ngocrong_max : 2000,
